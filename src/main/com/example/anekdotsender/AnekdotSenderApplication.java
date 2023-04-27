a726b00b078df5c9d60830a83df3cf48ac6c6bf3
MikeKoms <32240891+MikeKoms@users.noreply.github.com>
2023-04-27 21:04:53 +0300

    Create AnekdotSenderApplication.java
    add first file

---
+++ src/main/com/example/anekdotsender/AnekdotSenderApplication.java
@@ -0,0 +1,13 @@
+package com.example.anekdotsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnekdotSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnekdotSenderApplication.class, args);
+	}
+
+}
