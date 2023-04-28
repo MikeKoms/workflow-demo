1b7edafbcb218f7b7b895c24d2e92ed10f4f5135
MikeKoms <32240891+MikeKoms@users.noreply.github.com>
2023-04-28 20:39:48 +0300

    Update AnekdotsConfiguration.java

--- src/main/java/com/example/anekdotsender/AnekdotsConfiguration.java
+++ src/main/java/com/example/anekdotsender/AnekdotsConfiguration.java
@@ -4,16 +4,22 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import java.util.List;
+
 
 @Configuration
 @ConfigurationProperties(prefix = "anekdots")
 @Primary
-public class AnekdotsConfiguration {
+public class AnekdotsConfiguration{
 
     private String[] list;
 
-    public String[] getList() {
-        return list;
+
+    public String[] getList(){return list;}
+
+    public Integer method() {
+        Integer $aad = null;
+        return $aad;
     }
 
     public void setList(String[] list) {
